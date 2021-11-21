55a704963981461d08d60be7d13e169e87601fbe
James Clark <jamesc123@gmail.com>
2021-11-21 00:17:38 +0000

    Create seed.groovy

---
+++ jobs/seed.groovy
@@ -0,0 +1,26 @@
+def projectName = 'aieng-james-test'
+def repoUrl = 'https://github.com/sky-uk/aieng-james-test'
+def folderName = 'Builds'
+multibranchPipelineJob("/${folderName}/${projectName}") {
+  displayName "${projectName}"
+  description "desc"
+  branchSources {
+    branchSource {
+      source {
+        github {
+          id "${folderName}/${projectName}"
+          repositoryUrl "${repoUrl}"
+          repository "${projectName}"
+          repoOwner "sky-uk"
+          credentialsId "github-bot"
+          configuredByUrl true
+          traits {
+            gitHubBranchDiscovery {
+              strategyId(3)
+            }
+          }
+        }
+      }
+    }
+  }
+}

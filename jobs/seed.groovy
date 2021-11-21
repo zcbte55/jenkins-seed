531445f7001d4239bf68c93dd44c788c8ffb5682
James Clark <jamesc123@gmail.com>
2021-11-21 00:31:19 +0000

    Update seed.groovy

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,5 +1,5 @@
-def projectName = 'aieng-james-test'
-def repoUrl = 'https://github.com/sky-uk/aieng-james-test'
+def projectName = 'aieng-auth-gateway'
+def repoUrl = "https://github.com/sky-uk/${projectName}"
 def folderName = 'Builds'
 folder(folderName) {
 }

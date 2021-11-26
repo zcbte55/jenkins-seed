03a6508abec4a0558f3e8d859bf9324594efdad3
James Clark <jamesc123@gmail.com>
2021-11-26 21:01:57 +0000

    Update seed.groovy

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -10,24 +10,26 @@ def yaml = new Yaml(options)
 def config = yaml.load(fileContent)
 def repos = config.get('repos')
 
-repos.each {
-  repo ->
-    def name = repo.get('name')
-  def repoUrl = "https://github.com/sky-uk/${name}"
+repos.each { repo ->
+  def name = repo.get('name')
+  def repoUrl  = repo.get('url')
+  def urlParts = repo.split('/')
+  def repoOrg = urlParts[3]
+  def repoName = urlParts[4]
+  
   def folderName = 'Builds'
   folder(folderName) {}
 
   multibranchPipelineJob("/${folderName}/${name}") {
     displayName "${name}"
-    description "desc"
     branchSources {
       branchSource {
         source {
           github {
             id "${folderName}/${name}"
-            repositoryUrl "${repoUrl}"
-            repository "${name}"
-            repoOwner "sky-uk"
+            repositoryUrl repoUrl
+            repository repoName
+            repoOwner repoOrg
             credentialsId "github-bot"
             configuredByUrl true
             traits {
